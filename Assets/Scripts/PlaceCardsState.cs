29601594a203cd61af788a364c7ae30ce3c866fc
Caldas <cainacc@hotmail.com>
2025-10-19 22:05:46 -0300

    sin move on hand cards

--- Assets/Scripts/PlaceCardsState.cs
+++ Assets/Scripts/PlaceCardsState.cs
@@ -13,15 +13,15 @@ public class PlaceCardsState : BattleState
     {
         base.Update();
 
+        // Get references
+        DuelistController duelist = battleController.CurrentDuelist;
+        DeckController deck = duelist.deckController;
+        HandController hand = duelist.handController;
+        BattlefieldController battlefield = duelist.battlefieldController;
+        CardController[] handCardSlots = duelist.handController.CardSlots;
+        
         if (hasAnyCardBeenReleasedThisFrame)
         {
-            // Get references
-            DuelistController duelist = battleController.CurrentDuelist;
-            DeckController deck = duelist.deckController;
-            HandController hand = duelist.handController;
-            BattlefieldController battlefield = duelist.battlefieldController;
-            CardController[] handCardSlots = hand.CardSlots;
-
             // Detect if a card is near a slot to place it there
             for (int h = 0; h < handCardSlots.Length; h++)
             {
@@ -46,6 +46,52 @@ public class PlaceCardsState : BattleState
                     }
                 }
             }
+            
+        }
+
+        // Resolve wave movement of hand cards
+        {
+            // Check if any card is being held
+            bool hasHeldCard = false;
+            for (int c = 0; c < handCardSlots.Length; c++)
+            {
+                CardController card = handCardSlots[c];
+                bool isCardCompletellyInSlot = card.transform.parent == hand.slots[c];
+                if ( ! isCardCompletellyInSlot )
+                {
+                    hasHeldCard = true;
+                    break;
+                }
+            }
+
+            if (hasHeldCard)
+            {
+                // Reset position of cards that are not being held
+                for (int c = 0; c < handCardSlots.Length; c++)
+                {
+                    CardController card = handCardSlots[c];
+                    bool isCardCompletellyInSlot = card.transform.parent == hand.slots[c];
+                    if (isCardCompletellyInSlot)
+                    {
+                        card.ResetPosition();
+                    }
+                }
+            }
+            else
+            {
+                // Make cards do a smooth wave movement
+                for (int c = 0; c < handCardSlots.Length; c++)
+                {
+                    // 90 degrees offset per card: 0, 90, 180, 270
+                    float radians = (Mathf.PI / 2f) * c;
+                        
+                    // Move the card's transform up/down
+                    const float AMPLITUDE = 10.0f;
+                    const float SPEED = 3.0f;
+                    float yOffset = Mathf.Sin(Time.time * SPEED + radians) * AMPLITUDE;
+                    handCardSlots[c].transform.localPosition = new Vector3(0.0f, yOffset, 0.0f);
+                }
+            }
         }
         
         hasAnyCardBeenReleasedThisFrame = false;
@@ -54,6 +100,13 @@ public class PlaceCardsState : BattleState
     public override void OnExitState()
     {
         _hasPastFirstTurn = true;
+
+        // Reset all hand card positions
+        CardController[] handCardSlots = battleController.CurrentDuelist.handController.CardSlots;
+        for (int c = 0; c < handCardSlots.Length; c++)
+        {
+            handCardSlots[c].ResetPosition();
+        }
     }
     
     public override BattleState GetNextState()
